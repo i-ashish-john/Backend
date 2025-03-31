8512250c50ea6233a1fa3ec47b3e5c0b430ba09e
Ashish john <johnashish509@gmail.com>
2025-03-31 11:37:10 +0530

    Initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import  express,{ Application } from "express"
-const app:Application = express()
+import express, { Application } from "express"
+const app: Application = express()
 
 import cors from "cors"
 
@@ -11,26 +11,16 @@ import dotenv from 'dotenv';
  
 dotenv.config();
 
-
-
 app.use(express.json());
 
 app.use(cors({
-   origin: "http://localhost:3000/",  
+   origin: "http://localhost:3000",  
    credentials: true
 }));
 
 connectDB() 
 
+app.use("/api", user)
 
-
-app.use("/api",user)
-
-
-const PORT = process.env.PORT|| 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`==>>>> Server running on port ${PORT}`));
-
-
-
-
-
